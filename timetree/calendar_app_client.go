c652bb32518dee9d5f68dcf207989a9c8a17905a
gotokatsuya <katsuya.goto.dev@gmail.com>
2020-11-30 15:46:51 +0900

    add: update event

--- timetree/calendar_app_client.go
+++ timetree/calendar_app_client.go
@@ -45,21 +45,41 @@ type CreateCalendarEventRequestData struct {
 	Attributes EventAttributes `json:"attributes"`
 }
 
-type EventAttributes struct {
-	Category      string `json:"category"`
-	Title         string `json:"title"`
-	AllDay        bool   `json:"all_day"`
-	StartAt       string `json:"start_at"`
-	StartTimezone string `json:"start_timezone,omitempty"`
-	EndAt         string `json:"end_at"`
-	EndTimezone   string `json:"end_timezone,omitempty"`
-	Description   string `json:"description,omitempty"`
-	Location      string `json:"location,omitempty"`
-	URL           string `json:"url,omitempty"`
-}
-
 // CreateCalendarEventResponse type
 type CreateCalendarEventResponse struct {
+	api.ErrorResponse
+	Data struct {
+		ID         string          `json:"id"`
+		Type       string          `json:"type"`
+		Attributes EventAttributes `json:"attributes"`
+	} `json:"data"`
+}
+
+func (c *CalendarAppClient) UpdateCalendarEvent(ctx context.Context, eventID string, req *UpdateCalendarEventRequest) (*UpdateCalendarEventResponse, *http.Response, error) {
+	path := fmt.Sprintf("/calendar/events/%s", eventID)
+	httpReq, err := c.client.NewRequest(http.MethodPut, path, req)
+	if err != nil {
+		return nil, nil, err
+	}
+	resp := new(UpdateCalendarEventResponse)
+	httpResp, err := c.client.Do(ctx, httpReq, resp)
+	if err != nil {
+		return nil, httpResp, err
+	}
+	return resp, httpResp, nil
+}
+
+// UpdateCalendarEventRequest type
+type UpdateCalendarEventRequest struct {
+	Data UpdateCalendarEventRequestData `json:"data"`
+}
+
+type UpdateCalendarEventRequestData struct {
+	Attributes EventAttributes `json:"attributes"`
+}
+
+// UpdateCalendarEventResponse type
+type UpdateCalendarEventResponse struct {
 	api.ErrorResponse
 	Data struct {
 		ID         string          `json:"id"`
@@ -79,4 +99,20 @@ func (c *CalendarAppClient) DeleteCalendarEvent(ctx context.Context, eventID str
 		return httpResp, err
 	}
 	return httpResp, nil
+}
+
+// -- common types --
+
+// EventAttributes type
+type EventAttributes struct {
+	Category      string `json:"category"`
+	Title         string `json:"title"`
+	AllDay        bool   `json:"all_day"`
+	StartAt       string `json:"start_at"`
+	StartTimezone string `json:"start_timezone,omitempty"`
+	EndAt         string `json:"end_at"`
+	EndTimezone   string `json:"end_timezone,omitempty"`
+	Description   string `json:"description,omitempty"`
+	Location      string `json:"location,omitempty"`
+	URL           string `json:"url,omitempty"`
 }
